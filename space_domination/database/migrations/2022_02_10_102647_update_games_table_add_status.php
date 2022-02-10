3dd65af9e3305a70c909b650c43ab0b32dc9d072
WorthitIT <68585173+WorthitIT@users.noreply.github.com>
2022-02-10 11:32:20 +0100

    Added status and armageddon

---
+++ space_domination/database/migrations/2022_02_10_102647_update_games_table_add_status.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateGamesTableAddStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('games', function (Blueprint $table) {
+          
+            $table->enum("status", ['open','running','armaggedon','finished'])->default('open');
+            $table->timestamp("armaggedon_time")->nullable();
+            $table->integer('armaggedon_duration')->default(7);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
