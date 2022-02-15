90cf647ec934ed65c0d8bb8d8272d21339db140d
WorthitIT <68585173+WorthitIT@users.noreply.github.com>
2022-02-15 14:28:07 +0100

    change login form

--- space_domination/resources/js/Components/LoginGlassBlock.vue
+++ space_domination/resources/js/Components/LoginGlassBlock.vue
@@ -1,25 +1,20 @@
 <template>
 
-<form action="playthegame" method="post">
 
   <div class="blue-glass w-full">
-    <label for="username" class=" text-orange"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="userame" required>
-
-    <label for="password" class=" text-orange"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" required>
-
-   
-    <label class="w-full">
-      <input type="checkbox" class=" text-orange" checked="checked" name="remember"> Remember me
-    </label>
-    <div class="w-full text-gray-800">
-  <button type="submit" class="py-2 px-2 bg-pink text-black m-0.5 rounded-lg">Login</button>
-    <a type="button" class="cancelbtn py-2 px-2 bg-green m-0.5 rounded-lg ">Register</a>
-</div>
-    <span class="password w-full"><a href="#">Forgot password?</a></span>
+    <login-component/>
   </div>
-  
-</form>
-<!-- <link rel="stylesheet" type="text/css" href="@/css/app.css"> -->
     </template>
+<script>
+
+  import LoginComponent from "@/Components/LoginComponent.vue";
+
+export default {
+  setup() {
+    
+  },
+  components:{
+    LoginComponent
+  }
+}
+</script>
