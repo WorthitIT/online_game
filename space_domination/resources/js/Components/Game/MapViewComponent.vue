f567c27307471e3c88d9f565235a61391c0a6ad7
AlexKlasser-HUB <69719966+AlexKlasser-HUB@users.noreply.github.com>
2022-02-23 11:53:01 +0100

    adding lines
    added code for lines

--- space_domination/resources/js/Components/Game/MapViewComponent.vue
+++ space_domination/resources/js/Components/Game/MapViewComponent.vue
@@ -2,17 +2,11 @@
     <div style="height: 90vh" class="w-full">
 
   <Renderer height="500" resize ref="renderer">
-<<<<<<< Updated upstream
-    <Camera :position="{ z: 1000 }" />
-    <Scene>
-        <AmbientLight :intensity="1" />
-=======
     <Camera ref="camera" :position="{ z: 1000 }" />
     <Scene ref="scene">
         <AmbientLight :intensity="0.5" />
       <PointLight :position="{ y: 500, z: 0 }" />
       <PointLight color="#ff6000" :intensity="100" :position="{ y: -500, z: 100, x: 100 }" />
->>>>>>> Stashed changes
      <Sphere v-for="star,index in $page.props.stars" :key="index"  :radius="star.radius" :position="{ x: star.x, y: star.y, z: star.z}">
          <BasicMaterial :color="star.color" />
       </Sphere>
@@ -23,11 +17,6 @@
 </template>
 
 <script>
-<<<<<<< Updated upstream
-import { Sphere, Camera, BasicMaterial, PointLight, Renderer, Scene,AmbientLight } from 'troisjs';
-export default {
-  components: { Sphere, Camera, BasicMaterial, PointLight, Renderer, Scene,AmbientLight },
-=======
 import {Sphere, Camera, PhongMaterial, PointLight, Renderer, Scene,AmbientLight } from 'troisjs';
 import { Vector3, BufferGeometry, LineBasicMaterial, Line} from 'three';
 export default {
@@ -50,7 +39,6 @@ renderer.render(scene, camera);
 },
   props: ["stars"],
   components: {  Sphere, Camera, PhongMaterial, PointLight, Renderer, Scene,AmbientLight, Vector3, BufferGeometry, Line },
->>>>>>> Stashed changes
 };
 
 </script> 
